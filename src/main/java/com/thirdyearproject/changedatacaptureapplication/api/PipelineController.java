20b11a587d547f48e97ba36084d134ff2779cc87
Peter Hamer <peter.hamerrr@gmail.com>
2024-02-02 13:47:23 +0000

    Add pipeline halt request.

--- src/main/java/com/thirdyearproject/changedatacaptureapplication/api/PipelineController.java
+++ src/main/java/com/thirdyearproject/changedatacaptureapplication/api/PipelineController.java
@@ -4,12 +4,14 @@ import com.thirdyearproject.changedatacaptureapplication.api.model.PipelineConfi
 import com.thirdyearproject.changedatacaptureapplication.engine.PipelineInitializer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/pipeline")
 public class PipelineController {
 
   private final PipelineInitializer pipelineInitializer;
@@ -19,8 +21,13 @@ public class PipelineController {
     this.pipelineInitializer = pipelineInitializer;
   }
 
-  @GetMapping("/pipeline/run")
+  @PostMapping("/run")
   public void runPipeline(@RequestBody PipelineConfiguration config) {
     pipelineInitializer.runPipeline(config);
+  }
+
+  @PostMapping("/halt")
+  public void haltPipeline() {
+    pipelineInitializer.haltPipeline();
   }
 }
