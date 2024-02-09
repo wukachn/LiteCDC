6cb4a44dffdf6a41aee66e60207d55fe46cd68b3
Peter Hamer <peter.hamerrr@gmail.com>
2024-02-09 10:07:30 +0000

    Remove unused publication/replication slot create statements from streamer.

--- src/main/java/com/thirdyearproject/changedatacaptureapplication/engine/produce/streaming/PostgresStreamer.java
+++ src/main/java/com/thirdyearproject/changedatacaptureapplication/engine/produce/streaming/PostgresStreamer.java
@@ -10,10 +10,6 @@ import org.postgresql.replication.PGReplicationStream;
 
 @Slf4j
 public class PostgresStreamer extends Streamer {
-  private static final String CREATE_PUBLICATION =
-      "CREATE PUBLICATION cdc_publication FOR ALL TABLES;";
-  private static final String CREATE_REPLICATION_SLOT =
-      "SELECT pg_create_logical_replication_slot('cdc_replication_slot', 'cdc_publication');";
 
   private final PgOutputMessageDecoder pgOutputMessageDecoder;
   private final JdbcConnection replicationConnection;
@@ -46,20 +42,6 @@ public class PostgresStreamer extends Streamer {
       }
     } catch (Exception e) {
       log.error(e.getMessage(), e);
-    }
-  }
-
-  private void createPublication() throws SQLException {
-    log.info("Creating Publication with Statement: {}", CREATE_PUBLICATION);
-    try (var stmt = jdbcConnection.getConnection().createStatement()) {
-      stmt.execute(CREATE_PUBLICATION);
-    }
-  }
-
-  private void createReplicationSlot() throws SQLException {
-    log.info("Creating Replication Slot with Statement: {}", CREATE_REPLICATION_SLOT);
-    try (var stmt = jdbcConnection.getConnection().createStatement()) {
-      stmt.execute(CREATE_REPLICATION_SLOT);
     }
   }
 }
